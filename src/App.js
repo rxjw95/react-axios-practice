39ebf64edacfb63acec9e2729e432e393148b2aa
Jangwook Ryu <rxjw95@gmail.com>
2021-08-20 18:13:38 +0900

    Add: useReducer를 활용하여 3개의 state를 묶어서 로직 분리

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import './App.css';
 import Users from './Users';
 
 function App() {
-  return (
-    <Users/>
-  );
+    return (
+        <div>
+            <Users />
+            <button onClick={() => window.location.reload()}>
+                다시 불러오기
+            </button>
+        </div>
+    );
 }
 
 export default App;
--- src/Users.js
+++ src/Users.js
@@ -1,40 +1,71 @@
-import React, {useState, useEffect} from 'react';
+import React, { useReducer, useEffect } from 'react';
 import axios from 'axios';
 
-const Users = () => {
-  const [users, setUsers] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+const initialState = {
+    users: null,
+    loading: false,
+    error: null,
+};
 
-  useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        setUsers(null);
-        setError(null);
-        setLoading(true);
-        const response = await axios.get('https://jsonplaceholder.typicode.com/users/');
-        setUsers(response.data);
-      } catch(e) {
-        setError(e);
-      }
-      setLoading(false);
+function reducer(state, action) {
+    switch (action.type) {
+        case 'LOADING':
+            return {
+                loading: true,
+                users: null,
+                error: null,
+            };
+        case 'SUCCESS':
+            return {
+                loading: false,
+                users: action.users,
+                error: null,
+            };
+        case 'ERROR':
+            return {
+                loading: false,
+                users: null,
+                error: action.error,
+            };
+        default:
+            throw new Error('unhandled action type');
     }
-
-    fetchUsers();
-  }, []);
-
-  if(loading) return <div>로딩중</div>;
-  if(error) return <div>에러가 발생했습니다.</div>;
-  if(!users) return null;
-
-  return (
-    <ul>
-      {users.map(user => <li key={user.id}>
-        {users.username} {user.name}
-      </li>)}
-    </ul>
-  )
 }
 
-export default Users;
+const Users = () => {
+    const [state, dispatch] = useReducer(reducer, initialState);
 
+    const { users, loading, error } = state;
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+            dispatch({ type: 'LOADING' });
+            try {
+                const response = await axios.get(
+                    'https://jsonplaceholder.typicode.com/users/',
+                );
+                dispatch({ type: 'SUCCESS', users: response.data });
+            } catch (e) {
+                dispatch({ type: 'ERROR', error: e });
+            }
+        };
+
+        fetchUsers();
+    }, []);
+
+    if (loading) return <div>로딩중</div>;
+    if (error) return <div>에러가 발생했습니다.</div>;
+    if (!users) return null;
+
+    return (
+        <ul>
+            {users.map((user) => (
+                <li key={user.id}>
+                    {users.username} {user.name}
+                </li>
+            ))}
+        </ul>
+    );
+};
+
+export default Users;
